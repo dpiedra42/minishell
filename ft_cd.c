b47e2ee0fbb92bd31ea10793d162b3c08f91afef
deanna <42.dpiedra@gmail.com>
2021-03-05 14:41:03 +0100

    finished cd

--- ft_cd.c
+++ ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 11:49:01 by dpiedra           #+#    #+#             */
-/*   Updated: 2021/03/04 17:16:02 by dpiedra          ###   ########.fr       */
+/*   Updated: 2021/03/05 14:39:30 by dpiedra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,25 @@ void	ft_error(char *str, int status)
 {
 	g_status = status;
 	ft_putstr_fd(str, 0);
+}
+
+int		cd_path(char **input, t_data *data)
+{
+	if (chdir(input[1]) == -1)
+		return (0);
+	change_dir(data, input[1]);
+	return (1);
+}
+
+int		cd_minus(t_data *data)
+{
+	int index;
+	
+	index = env_index(data, "OLDPWD=");
+	if (index == -1 || chdir((strchr(data->env[index], '=') + 1)) == -1 )
+		return (0);
+	change_dir(data, "-");
+	return (1);
 }
 
 int		cd_empty(t_data *data)
@@ -38,5 +57,15 @@ void	ft_cd(char **input, t_data *data)
 		if(!cd_empty(data))
 			return (ft_error("cd: HOME not defined\n", 1));
 	}
-	
+	else if (ft_strcmp(input[1], "-") == 0)
+	{
+		if (!cd_minus(data))
+			return (ft_error("cd: OLDPWD is not defined\n", 1));
+	}
+	else
+	{
+		if (!cd_path(input, data))
+			return (ft_error("cd: no such file or directory\n", 1));
+	}
+	g_status = 0;
 }
