6d9d71be36c60128b877e75299e553ba1a9a86dd
deanna <42.dpiedra@gmail.com>
2021-03-08 18:54:00 +0100

    $ variable

--- parsing_var.c
+++ parsing_var.c
@@ -6,7 +6,7 @@
 /*   By: dpiedra <dpiedra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/08 17:41:45 by dpiedra           #+#    #+#             */
-/*   Updated: 2021/03/08 18:47:19 by dpiedra          ###   ########.fr       */
+/*   Updated: 2021/03/08 18:53:27 by dpiedra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,28 +24,34 @@ static int	var_len(char *str)
 	return (len);
 }
 
-// static char	*dup_value(char *str)
-// {
-// 	int		maxlen;
-// 	char	*value;
-// 	int		i;
-// 	int		j;
+static void		add_escaped_char(char *src, char *dst, int *i, int *j)
+{
+	dst[(*j)++] = '\\';
+	dst[(*j)++] = src[(*i)++];
+}
 
-// 	maxlen = ft_strlen(str) * 2;
-// 	value = malloc((maxlen + 1) * sizeof(char));
-// 	i = 0;
-// 	j = 0;
-// 	while (str[i])
-// 	{
-// 		if (str[i] == '\\' || str[i] == '$' || str[i] == '"' ||
-// 			str[i] == '\'' || str[i] == '>' || str[i] == '<')
-// 			add_escaped_char(str, value, &i, &j);
-// 		else
-// 			value[j++] = str[i++];
-// 	}
-// 	value[j] = '\0';
-// 	return (value);
-// }
+static char	*dup_value(char *str)
+{
+	int		maxlen;
+	char	*value;
+	int		i;
+	int		j;
+
+	maxlen = ft_strlen(str) * 2;
+	value = malloc((maxlen + 1) * sizeof(char));
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == '\\' || str[i] == '$' || str[i] == '"' ||
+			str[i] == '\'' || str[i] == '>' || str[i] == '<')
+			add_escaped_char(str, value, &i, &j);
+		else
+			value[j++] = str[i++];
+	}
+	value[j] = '\0';
+	return (value);
+}
 
 static char	*get_val(char *name, t_data *data)
 {
@@ -61,7 +67,7 @@ static char	*get_val(char *name, t_data *data)
 		while (env[i][j] && env[i][j] != '=' && env[i][j] == name[j])
 			j++;
 		if (env[i][j] == '=' && !name[j])
-			return (0); // return (dup_value(&env[i][j + 1]));
+			return (dup_value(&env[i][j + 1]));
 		i++;
 	}
 	return (NULL);
@@ -72,8 +78,8 @@ void		find_variable(char **command, int *i, t_data *data)
 	int		len;
 	char	*var_value;
 	char	*var_name;
-	// char	*new_command;
-	// char	*tmp;
+	char	*new_command;
+	char	*tmp;
 
 	len = var_len(&(command[0][*i +1]));
 	var_name = ft_substr(*command, *i + 1, len);
@@ -81,4 +87,17 @@ void		find_variable(char **command, int *i, t_data *data)
 		var_value = ft_itoa(g_status);
 	else if (len)
 		var_value = get_val(var_name, data);
+	else
+		var_value = ft_strdup("$");
+	free(var_name);
+	new_input = ft_substr(*command, 0, *i);
+	tmp = ft_strjoin(new_input, var_value);
+	free(new_input);
+	new_input = ft_strjoin(tmp, &(command[0][*i + 1 + len]));
+	len = ft_strlen(var_value);
+	free(tmp);
+	free(var_value);
+	free(*command);
+	*command = new_input;
+	*i += len - 1;
 }
